31311733d98b07925ccc20b8465c05412aecda87
Juan Felipe <jfrayop@gmail.com>
2020-07-07 23:40:29 -0500

    Updates

---
+++ precache-manifest.0258b9b5f3a2cc5a352fd44f04709366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbce5c495f25e37a6befbe138d84f048",
+    "url": "/comicbook-app/index.html"
+  },
+  {
+    "revision": "388cbea8c6a1201e630f",
+    "url": "/comicbook-app/static/css/main.92c4b1c9.chunk.css"
+  },
+  {
+    "revision": "74483d9d64de8e479d6c",
+    "url": "/comicbook-app/static/js/2.fdcd255d.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/comicbook-app/static/js/2.fdcd255d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388cbea8c6a1201e630f",
+    "url": "/comicbook-app/static/js/main.6c2dcfd2.chunk.js"
+  },
+  {
+    "revision": "104fb77a07abe87e22c9",
+    "url": "/comicbook-app/static/js/runtime-main.dc0259bc.js"
+  }
+]);
